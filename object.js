98a01b7aa128d54e957c99ae58cbdd7a53889859
Abdul-Jobber-2407 <arjobbar2019@gmail.com>
2024-11-02 12:24:12 +0600

    object

--- object.js
+++ object.js
@@ -4,10 +4,24 @@ const student = {
     address : true,
     isSingle: true,
     friends: ['Apu', 'Raaz', 'Salman', 'Nirob'],
+    movies : [{name : 'no.1', year : 2015}, { name:'King Khan', year : 2018}],
+    act : function(){
+        console.log('acting like sakib khan')
+    },
     car: {
         brand : 'tesla', 
         price: 5000000,
         made: 2025,
+        manufacturer:{
+            name : 'tesla',
+            ceo: 'Elon Mask',
+            country: 'USA',
+        }
     }
 }
-console.log(student);
+// console.log(student);
+// console.log(student.friends);
+
+console.log(student.act);
+
+student.act();
