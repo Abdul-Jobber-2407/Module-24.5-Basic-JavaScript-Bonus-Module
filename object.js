4cce74c2c5a2f3aaf140a619e5c2b971d495272f
Abdul-Jobber-2407 <arjobbar2019@gmail.com>
2024-11-02 12:13:05 +0600

    first commit

---
+++ object.js
@@ -0,0 +1,13 @@
+const student = {
+    name : 'sakib khan',
+    id : 121,
+    address : true,
+    isSingle: true,
+    friends: ['Apu', 'Raaz', 'Salman', 'Nirob'],
+    car: {
+        brand : 'tesla', 
+        price: 5000000,
+        made: 2025,
+    }
+}
+console.log(student);
